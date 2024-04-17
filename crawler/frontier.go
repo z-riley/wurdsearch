e1bd81e5718030d91cc608f90d5cea6b20a44ee3
zac460 <zac460@hotmail.co.uk>
2024-04-17 21:24:45 +0100

    Implement basic diagnostics for frontier

--- crawler/frontier.go
+++ crawler/frontier.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/url"
+
 	"github.com/enriquebris/goconcurrentqueue"
 )
 
@@ -12,4 +14,35 @@ func newFrontier() *frontier {
 	return &frontier{
 		queue: goconcurrentqueue.NewFIFO(),
 	}
+}
+
+// TODO: make sure new links sent to frontier aren't already in the frontier
+
+// getAll returns every item in the frontier as a string. Warning: this function isn't thread safe
+func (f *frontier) getAll() ([]string, error) {
+	var items []string
+
+	for i := 0; i < f.queue.GetLen(); i++ {
+		item, err := f.queue.Get(i)
+		if err != nil {
+			return items, err
+		}
+
+		items = append(items, item.(string))
+	}
+
+	return items, nil
+}
+
+// countOccurrances returns a map of the most common strings in a slice and their respective counts
+func countOccurrances(urls []string) (map[string]int, error) {
+	result := make(map[string]int)
+	for _, URL := range urls {
+		parsedUrl, err := url.Parse(URL)
+		if err != nil {
+			return result, err
+		}
+		result[parsedUrl.Host] += 1
+	}
+	return result, nil
 }
---
+++ crawler/frontier_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+)
+
+
+func TestGetAll(t *testing.T) {
+	f := newFrontier()
+	for _, url := range sampleCrawledUrls() {
+		f.queue.Enqueue(url)
+	}
+
+	contents, err := f.getAll()
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(contents, sampleCrawledUrls()) {
+		t.Error("Expected out different to known input")
+	}
+}
+
+func TestCountOccurrances(t *testing.T) {
+	actual, err := countOccurrances(sampleCrawledUrls())
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(actual, map[string]int{
+		"reddit.com":          62,
+		"www.telegraph.co.uk": 2,
+	}) {
+		t.Error("Actaul not not equal expected")
+	}
+}
+
+func sampleCrawledUrls() []string {
+	return []string{
+		"https://reddit.com/r/popular/hot/?geo_filter=us",
+		"https://reddit.com/r/popular/hot/?geo_filter=ar",
+		"https://reddit.com/r/popular/hot/?geo_filter=au",
+		"https://reddit.com/r/popular/hot/?geo_filter=bg",
+		"https://reddit.com/r/popular/hot/?geo_filter=ca",
+		"https://reddit.com/r/popular/hot/?geo_filter=cl",
+		"https://reddit.com/r/popular/hot/?geo_filter=co",
+		"https://reddit.com/r/popular/hot/?geo_filter=hr",
+		"https://reddit.com/r/popular/hot/?geo_filter=cz",
+		"https://reddit.com/r/popular/hot/?geo_filter=fi",
+		"https://reddit.com/r/popular/hot/?geo_filter=fr",
+		"https://reddit.com/r/popular/hot/?geo_filter=de",
+		"https://reddit.com/r/popular/hot/?geo_filter=gr",
+		"https://reddit.com/r/popular/hot/?geo_filter=hu",
+		"https://reddit.com/r/popular/hot/?geo_filter=is",
+		"https://reddit.com/r/popular/hot/?geo_filter=in",
+		"https://reddit.com/r/popular/hot/?geo_filter=ie",
+		"https://reddit.com/r/popular/hot/?geo_filter=it",
+		"https://reddit.com/r/popular/hot/?geo_filter=jp",
+		"https://reddit.com/r/popular/hot/?geo_filter=my",
+		"https://reddit.com/r/popular/hot/?geo_filter=mx",
+		"https://reddit.com/r/popular/hot/?geo_filter=nz",
+		"https://reddit.com/r/popular/hot/?geo_filter=ph",
+		"https://reddit.com/r/popular/hot/?geo_filter=pl",
+		"https://reddit.com/r/popular/hot/?geo_filter=pt",
+		"https://reddit.com/r/popular/hot/?geo_filter=pr",
+		"https://reddit.com/r/popular/hot/?geo_filter=ro",
+		"https://reddit.com/r/popular/hot/?geo_filter=rs",
+		"https://reddit.com/r/popular/hot/?geo_filter=sg",
+		"https://reddit.com/r/popular/hot/?geo_filter=es",
+		"https://reddit.com/r/popular/hot/?geo_filter=se",
+		"https://reddit.com/r/popular/hot/?geo_filter=tw",
+		"https://reddit.com/r/popular/hot/?geo_filter=th",
+		"https://reddit.com/r/popular/hot/?geo_filter=tr",
+		"https://reddit.com/r/popular/hot/?geo_filter=gb",
+		"https://reddit.com/?feedViewType=cardView",
+		"https://reddit.com/?feedViewType=compactView",
+		"https://reddit.com/r/unitedkingdom/comments/1c69zby/jk_rowling_gets_apology_from_journalist_after/",
+		"https://reddit.com/r/unitedkingdom/",
+		"https://reddit.com/r/unitedkingdom/",
+		"https://reddit.com/r/unitedkingdom/comments/1c69zby/jk_rowling_gets_apology_from_journalist_after/",
+		"https://www.telegraph.co.uk/news/2024/04/16/jk-rowling-holocaust-denier-allegation-rivkah-brown-novara/",
+		"https://www.telegraph.co.uk/news/2024/04/16/jk-rowling-holocaust-denier-allegation-rivkah-brown-novara/",
+		"https://reddit.com/r/AskUK/comments/1c66aak/whats_the_worst_work_gift_youve_ever_received/",
+		"https://reddit.com/r/AskUK/",
+		"https://reddit.com/r/AskUK/",
+		"https://reddit.com/r/AskUK/comments/1c66aak/whats_the_worst_work_gift_youve_ever_received/",
+		"https://reddit.com/r/AskUK/comments/1c66aak/whats_the_worst_work_gift_youve_ever_received/",
+		"https://reddit.com/r/london/comments/1c6c87e/please_help_find_my_missing_friend/",
+		"https://reddit.com/r/london/",
+		"https://reddit.com/r/london/",
+		"https://reddit.com/r/london/comments/1c6c87e/please_help_find_my_missing_friend/",
+		"https://reddit.com/r/DestinyTheGame",
+		"https://reddit.com/r/anime",
+		"https://reddit.com/r/destiny2",
+		"https://reddit.com/r/FortNiteBR",
+		"https://reddit.com/r/dndnext",
+		"https://reddit.com/r/buildapc",
+		"https://reddit.com/r/techsupport",
+		"https://reddit.com/r/jailbreak",
+		"https://reddit.com/r/LivestreamFail",
+		"https://reddit.com/r/legaladvice",
+		"https://reddit.com/r/LifeProTips",
+		"https://reddit.com/r/AskCulinary",
+	}
+}
