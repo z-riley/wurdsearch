c221f3aa4e500e268409c5411117f05e7d6f809e
zac460 <zac460@hotmail.co.uk>
2024-05-16 20:46:24 +0100

    Revert "Stop crawler if any crawling agents fail"
    This reverts commit 3da9b363a698b631622c618dfed1a29f66e25cb5.

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -64,7 +64,7 @@ func (c *Crawler) SetSeeds(urls []string) error {
 func (c *Crawler) CrawlForever() {
 	for {
 		if err := c.crawlingSequence(); err != nil {
-			log.Fatal().Err(err).Msg("Crawl failed")
+			log.Warn().Err(err).Msg("Crawl failed")
 		}
 	}
 }
