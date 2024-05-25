d78250d8ad96d070d1852db29a135bb7fb85687b
zac460 <zac460@hotmail.co.uk>
2024-05-25 22:33:39 +0100

    Parallelise word indexing to speed up 5x

--- indexer/wordindex.go
+++ indexer/wordindex.go
@@ -1,15 +1,17 @@
 package indexer
 
 import (
-	"fmt"
 	"regexp"
 	"strings"
+	"sync"
 
 	"github.com/rs/zerolog/log"
 	"github.com/zac460/turdsearch/common/lemmatiser"
 	"github.com/zac460/turdsearch/common/stopwords"
 	"github.com/zac460/turdsearch/common/store"
 )
+
+const indexerWorkers = 10
 
 type WordIndexer struct {
 	lemmatiser *lemmatiser.Lemmatiser
@@ -42,6 +44,10 @@ func (w *WordIndexer) GenerateWordIndex(collectionName string) error {
 	}
 	count := 0
 
+	// Set up semaphore
+	var wg sync.WaitGroup
+	sem := make(chan struct{}, indexerWorkers)
+
 	for {
 		pageData, more, err := w.db.NextPageData()
 		if err != nil {
@@ -54,25 +60,35 @@ func (w *WordIndexer) GenerateWordIndex(collectionName string) error {
 		count++
 		log.Info().Msgf("Generating word index. Progress: %d/%d", count, length)
 
-		// Update word index for each word on the page
-		wordCounts := make(map[string]uint)
-		words := sanitiseString(strings.ToLower(pageData.Content))
-		for _, word := range words {
-			// Ignore stop words since they'll never be searched for anyway
-			if stopwords.IsStopWord(word) {
-				continue
+		wg.Add(1)
+		sem <- struct{}{} // take from semaphore
+
+		go func(pageData store.PageData) {
+			defer wg.Done()
+			defer func() { <-sem }() // give to semphore
+
+			// Update word index for each word on the page
+			wordCounts := make(map[string]uint)
+			words := sanitiseString(strings.ToLower(pageData.Content))
+			for _, word := range words {
+				// Ignore stop words since they'll never be searched for anyway
+				if stopwords.IsStopWord(word) {
+					continue
+				}
+				// Only store lemmas
+				lemmatisedWord := w.lemmatiser.Lemmatise(word)
+				wordCounts[lemmatisedWord] += 1
 			}
-			// Only store lemmas
-			lemmatisedWord := w.lemmatiser.Lemmatise(word)
-			wordCounts[lemmatisedWord] += 1
-		}
-		if len(wordCounts) > 0 {
-			err = w.db.UpdateWordReferences(pageData.Url, wordCounts, uint(len(words)))
-			if err != nil {
-				return fmt.Errorf("Failed to update word references for %s: %v", pageData.Url, err)
+			if len(wordCounts) > 0 {
+				err = w.db.UpdateWordReferences(pageData.Url, wordCounts, uint(len(words)))
+				if err != nil {
+					log.Error().Msgf("Failed to update word references for %s: %v", pageData.Url, err)
+				}
 			}
-		}
+		}(pageData)
 	}
+
+	wg.Wait()
 
 	return nil
 }
